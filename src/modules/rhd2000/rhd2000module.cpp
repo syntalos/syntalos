08914ecf769d9bc5d6d182d14af1c44a32c966e6
Matthias Klumpp <matthias@tenstral.net>
2020-05-19 14:13:41 +0200

    trivial: Revert RHD2000 tolerance change

--- src/modules/rhd2000/rhd2000module.cpp
+++ src/modules/rhd2000/rhd2000module.cpp
@@ -149,8 +149,9 @@ bool Rhd2000Module::prepare(const TestSubject &)
     clockSync->setStrategies(TimeSyncStrategy::WRITE_TSYNCFILE);
     clockSync->setTimeSyncBasename(intanBaseFilename);
 
-    // permit 1ms tolerance
-    clockSync->setTolerance(std::chrono::microseconds(1000));
+    // permit 2ms tolerance - this was a very realistic tolerance to achieve in tests,
+    // while lower values resulted in constant adjustment attempts
+    clockSync->setTolerance(std::chrono::microseconds(2000));
 
     // we only permit calibration with the very first data block - this seems to be sufficient and
     // yielded the best results (due to device and USB buffering, the later data blocks are more
