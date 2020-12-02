15cbbfcdd49806ae8a81e8184213dde238931673
Matthias Klumpp <matthias@tenstral.net>
2020-12-02 18:21:55 +0100

    rhd2000: Set tolerance of 1.5ms

--- src/modules/rhd2000/rhd2000module.cpp
+++ src/modules/rhd2000/rhd2000module.cpp
@@ -149,9 +149,9 @@ bool Rhd2000Module::prepare(const TestSubject &)
     clockSync->setStrategies(TimeSyncStrategy::WRITE_TSYNCFILE);
     clockSync->setTimeSyncBasename(intanBaseFilename, dstore->collectionId());
 
-    // permit 2ms tolerance - this was a very realistic tolerance to achieve in tests,
+    // permit 1.5ms tolerance - this was a very realistic tolerance to achieve in tests,
     // while lower values resulted in constant adjustment attempts
-    clockSync->setTolerance(std::chrono::microseconds(2000));
+    clockSync->setTolerance(std::chrono::microseconds(1500));
 
     // we only permit calibration with the very first data block - this seems to be sufficient and
     // yielded the best results (due to device and USB buffering, the later data blocks are more
