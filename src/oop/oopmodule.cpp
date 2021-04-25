5544e949f2dfd26a6d3c04e74dd063646f4d701e
Matthias Klumpp <matthias@tenstral.net>
2021-04-25 02:07:14 +0200

    oopm: Find Python worker in the right location when installed

--- src/oop/oopmodule.cpp
+++ src/oop/oopmodule.cpp
@@ -272,8 +272,8 @@ void OOPModule::setWorkerBinaryPyWorker()
 {
     d->workerBinary = QStringLiteral("%1/pyworker/pyworker").arg(QCoreApplication::applicationDirPath());
     QFileInfo checkBin(d->workerBinary);
-    if (!checkBin.exists()) {
-        d->workerBinary = QStringLiteral("%1/../lib/syntalos/pyworker").arg(QCoreApplication::applicationDirPath());
+    if (!checkBin.exists() || d->workerBinary.startsWith("/usr/")) {
+        d->workerBinary = QStringLiteral("%1/pyworker").arg(SY_LIBDIR);
         QFileInfo fi(d->workerBinary);
         d->workerBinary = fi.canonicalFilePath();
     }
