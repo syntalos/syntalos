57938a5a75b303930c2cdf6594a5d140511fafd1
Matthias Klumpp <matthias@tenstral.net>
2022-01-21 18:46:33 +0100

    Improve license explanation in "About" dialog
    This clarifies the GPL/LGPL situation is for this program a bit better.

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -21,21 +21,25 @@
 #include "ui_aboutdialog.h"
 #include "config.h"
 
+#include <QFont>
 #include <QFontDatabase>
 
 const QString aboutDlgCopyInfo = QStringLiteral(
         "<html>"
-        "(c) 2016-2022 Matthias Klumpp"
+        "© 2016-2022 Matthias Klumpp"
         "<p>Developed at the Draguhn Group at Heidelberg University, Germany</p>"
         "<p>Syntalos is free software: you can redistribute it and/or modify "
-        "it under the terms of the GNU General Public License as published by "
-        "the Free Software Foundation, either version 3 of the License, or "
-        "(at your option) any later version.</p>"
+        "it under the terms of the GNU General Public License (GPL-3.0+) and "
+        "GNU Lesser General Public License (LGPL-3.0+) as published by the Free Software Foundation, "
+        "either version 3 of the License, or (at your option) any later version.</p>"
+        "<p>While the main application as a combined work falls under the GPL-3.0+ license, "
+        "Syntalos' plugin interface and in fact most of its code is licensed under the LGPL-3.0+ license.</p>"
         "<p>Syntalos is distributed in the hope that it will be useful, "
         "but WITHOUT ANY WARRANTY; without even the implied warranty of "
         "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
         "GNU General Public License for more details.</p>"
-        "<p>Icons are based on the Breeze Iconset by the <a href=\"https://kde.org/\">KDE Community</a> [LGPLv3+]</p>"
+        "<p>Icons are based on the Breeze Iconset by the <a href=\"https://kde.org/\">KDE Community</a> [LGPLv3+]<br/>"
+        "ASCII art credit for this window: hjw `97</p>"
         "<h3>Modules:</h3>");
 
 AboutDialog::AboutDialog(QWidget *parent) :
@@ -72,6 +76,10 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->licenseTextBrowser->setPalette(palette);
     m_licenseText = aboutDlgCopyInfo;
     ui->licenseTextBrowser->setText(m_licenseText);
+
+    auto asciiArtFont = ui->asciiArtLabel->font();
+    asciiArtFont.setPointSize(8);
+    ui->asciiArtLabel->setFont(asciiArtFont);
 }
 
 AboutDialog::~AboutDialog()
