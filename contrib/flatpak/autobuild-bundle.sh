9a5155170e64b55b164247518aa785fdee333d0b
Matthias Klumpp <matthias@tenstral.net>
2022-05-04 20:04:32 +0200

    flatpak: Add some more GL/CL tweaks to get a working build
    Syntalos still won't work as Flatpak bundle, but at least it will start
    now.

---
+++ contrib/flatpak/autobuild-bundle.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+cd $(dirname "$0")
+
+build_shell=""
+if [ ! -z "$1" ]
+then
+	echo "Interactive shell at step: $1"
+	build_shell="--build-shell=$1"
+fi
+set -x
+
+flatpak-builder -y \
+	--install-deps-from=flathub \
+	--force-clean \
+	--repo=tmp_repo \
+	$build_shell \
+	build-dir \
+	io.github.bothlab.syntalos.yaml
+
+rm -f Syntalos.flatpak
+flatpak build-bundle \
+	--runtime-repo=https://flathub.org/repo/flathub.flatpakrepo \
+	tmp_repo/ \
+	Syntalos.flatpak \
+	io.github.bothlab.syntalos
+
+rm -r tmp_repo/
