327cf4e64722b62bdcf315ab86516a0e7db9f6a5
Matthias Klumpp <matthias@tenstral.net>
2026-03-15 12:38:31 +0100

    dist: Split package building into configure & build step

--- tests/ci/auto-build-deb.sh
+++ tests/ci/auto-build-deb.sh
@@ -26,16 +26,7 @@ eatmydata apt-get install -yq --no-install-recommends \
 # Build Debian package
 #
 
-git_commit=$(git log --pretty="format:%h" -n1)
-git_current_tag=$(git describe --abbrev=0 --tags 2> /dev/null || echo "v3.0.2")
-git_commit_no=$(git rev-list --count HEAD)
-upstream_version=$(echo "${git_current_tag}" | sed 's/^v\(.\+\)$/\1/;s/[-]/./g')
-upstream_version="$upstream_version+git$git_commit_no"
-
-set +x
-mv contrib/debian .
-dch --distribution "UNRELEASED"	--newversion="${upstream_version}" -b \
-    "New automated build from: ${upstream_version} - ${git_commit}"
+./tests/ci/configure-deb-build.sh "UNRELEASED" "deb"
 
 # Actually build the package.
 # Use a helper like `debspawn` or `debuild` when building manually!
