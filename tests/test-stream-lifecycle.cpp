59ab4ce9bd2083ab034f48e4b2fe0ad727da3a31
caenrigen <3199974-caenrigen@users.noreply.gitlab.com>
2026-09-01 12:32:19 +0200

    tests: Preserve suspended stream subscriptions on start

---
+++ tests/test-stream-lifecycle.cpp
@@ -0,0 +1,39 @@
+#include <QtTest>
+
+#include "datactl/frametype.h"
+#include "logging.h"
+#include "streams/stream.h"
+
+using namespace Syntalos;
+
+class TestStreamLifecycle : public QObject
+{
+    Q_OBJECT
+
+public:
+    TestStreamLifecycle()
+    {
+        initializeSyLogSystem(quill::LogLevel::Debug);
+    }
+
+private slots:
+    void suspensionSurvivesStart()
+    {
+        DataStream<Frame> stream;
+        auto subscription = stream.subscribe();
+
+        subscription->suspend();
+        stream.start();
+
+        for (uint64_t i = 0; i < 4; ++i)
+            stream.push(Frame(i));
+        QCOMPARE(subscription->approxPendingCount(), size_t(0));
+
+        subscription->resume();
+        stream.push(Frame(4));
+        QCOMPARE(subscription->approxPendingCount(), size_t(1));
+    }
+};
+
+QTEST_MAIN(TestStreamLifecycle)
+#include "test-stream-lifecycle.moc"
